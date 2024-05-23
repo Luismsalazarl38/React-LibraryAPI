9f4c3f92ccf242b90c36c9f0e6522d63335565f1
Luismsalazarl38 <luis.salazar1@utp.edu.co>
2024-05-22 23:19:56 -0500

    funcion de la hora de apertura y de cierre

--- client/src/pages/AdminStores.jsx
+++ client/src/pages/AdminStores.jsx
@@ -2,24 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export function AdminStores() {
-  // Estado para almacenar la lista de admins
   const [stores, setStores] = useState([]);
   const [formData, setFormData] = useState({
     id: '',
     address: '',
-    schedule: ''
+    openingTime: '',
+    closingTime: '',
   });
-
-  // Estado para controlar el índice del admin actual
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Función para desplazarse al admin anterior
   const prevStore = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? stores.length - 1 : prevIndex - 1));
   };
 
-  // Función para desplazarse al siguiente admin
-  const nextStore  = () => {
+  const nextStore = () => {
     setCurrentIndex((prevIndex) => (prevIndex === stores.length - 1 ? 0 : prevIndex + 1));
   };
 
@@ -31,10 +27,11 @@ export function AdminStores() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      const schedule = `${formData.openingTime} - ${formData.closingTime}`;
       const userData = {
         address: formData.address,
-        schedule: formData.schedule
-    };
+        schedule,
+      };
       const response = await axios.patch(`http://localhost:8000/manage/stores/${formData.id}/`, userData);
       if (response) {
         window.location.reload();
@@ -46,42 +43,33 @@ export function AdminStores() {
     }
   };
 
-  // Función para cargar los admins desde el servidor
   useEffect(() => {
     axios.get('http://localhost:8000/manage/stores/')
       .then(response => {
         setStores(response.data);
         const currentStore = response.data[currentIndex];
         setFormData({
-            id: currentStore.id,
-            address: currentStore.address,
-            schedule: currentStore.schedule
-          });
+          id: currentStore.id,
+          address: currentStore.address,
+          openingTime: currentStore.schedule.split(' - ')[0],
+          closingTime: currentStore.schedule.split(' - ')[1],
+        });
       })
       .catch(error => {
         console.error('Error fetching stores:', error);
       });
-  }, [currentIndex]); // Ejecutar cuando el currentIndex cambie
+  }, [currentIndex]);
 
   return (
     <div className="container content">
-      {/* Encabezado de la página */}
       <h1>Manejo de Tiendas</h1>
-
-      {/* Contenido principal */}
       <div className="slider">
-        {/* Botón para desplazarse al admin anterior */}
         <button className="button" onClick={prevStore}>&#10094;</button>
-
-        {/* Detalles del admin actual */}
         <div className="book-details">
-          {/* Mostrar la imagen del libro si existe */}
           <p>ID Tienda: {stores[currentIndex]?.id}</p>
           <p>Dirección: {stores[currentIndex]?.address}</p>
           <p>Horario: {stores[currentIndex]?.schedule}</p>
-
         </div>
-        {/* Botón para desplazarse al siguiente admin */}
         <button className="button" onClick={nextStore}>&#10095;</button>
       </div>
       <br />
@@ -89,28 +77,47 @@ export function AdminStores() {
       <div>
         <h2>Actualizar información</h2>
         {formData && (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Dirección:
-            <input type="text" name="address" value={formData.address} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Horario:
-            <input type="text" name="schedule" value={formData.schedule} onChange={handleChange} />
-          </label>
-          <br />
-          <button type="submit">Actualizar</button>
-          <br />
-          <br />
-          <h3>No olvides enviar los datos completos</h3>
-        </form>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Dirección:
+              <input
+                type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Horario de apertura:
+              <input
+                type="time"
+                name="openingTime"
+                value={formData.openingTime}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Horario de cierre:
+              <input
+                type="time"
+                name="closingTime"
+                value={formData.closingTime}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <button type="submit">Actualizar</button>
+            <br />
+            <br />
+            <h3>No olvides enviar los datos completos</h3>
+          </form>
         )}
       </div>
       <br />
       <br />
       <br />
-      {/* Pie de página */}
       <footer className="footer">
         <p>Derechos de autor © 2024. Todos los derechos reservados.</p>
       </footer>
