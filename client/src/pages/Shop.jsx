19f96fd979d20742d1c690d490dcf376be3a466f
Luismsalazarl38 <luis.salazar1@utp.edu.co>
2024-05-30 23:14:08 -0500

    Compras con tarjetas seleccionadas

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importar estilos de Bootstrap
-import { Alert } from 'react-bootstrap'; // Importar el componente de alerta de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alert } from 'react-bootstrap';
 
 export function Shop() {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [client, setClient] = useState(null); // Agregamos el estado para el cliente
-  const [cardDetails, setCardDetails] = useState({}); // Estado para almacenar detalles de la tarjeta
-  const [selectedBookId, setSelectedBookId] = useState(null); // Estado para controlar qué tarjeta está seleccionada
-  const [alertMessage, setAlertMessage] = useState(""); // Estado para el mensaje de alerta
-  const [alertVariant, setAlertVariant] = useState(""); // Estado para el tipo de alerta (success, danger, etc.)
+  const [client, setClient] = useState(null);
+  const [cardDetails, setCardDetails] = useState({});
+  const [selectedBookId, setSelectedBookId] = useState(null);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertVariant, setAlertVariant] = useState("");
+  const [clientCards, setClientCards] = useState([]);
 
   useEffect(() => {
     // Obtener la información del cliente al cargar el componente
     axios.get('http://localhost:8000/users/clients/')
       .then(response => {
-        setClient(response.data[0]); // Suponiendo que solo hay un cliente y lo guardamos en el estado
+        setClient(response.data[0]);
+        fetchClientCards(response.data[0].id);
       })
       .catch(error => {
         console.error('Error fetching client:', error);
@@ -32,16 +34,24 @@ export function Shop() {
       });
   }, []);
 
+  const fetchClientCards = async (clientId) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/manage/cards/?client=${clientId}`);
+      setClientCards(response.data);
+    } catch (error) {
+      console.error('Error fetching client cards:', error);
+    }
+  };
+
   const handleReserve = async (bookId) => {
     try {
-      // Realizar la solicitud POST al endpoint de reservas
       const response = await axios.post('http://localhost:8000/manage/reservations/', {
         book: bookId,
         client: client.id,
         expired: false,
         date: new Date().toISOString()
       });
-  
+
       if (response.status === 201) {
         setAlertMessage('Reserva realizada con éxito');
         setAlertVariant('success');
@@ -55,13 +65,20 @@ export function Shop() {
       console.error('Error al realizar la reserva:', error);
     }
   };
-  
 
-  const handleBuy = async (bookId) => { // <-- Agrega bookId como argumento
+  const handleBuy = async (bookId, selectedCard) => {
     try {
       // Verificar si se ha cargado la información del cliente
       if (!client) {
         setAlertMessage('No se ha cargado la información del cliente.');
+        setAlertVariant('danger');
+        return;
+      }
+
+      // Verificar si el saldo de la tarjeta es suficiente
+      const book = books.find(b => b.id === bookId);
+      if (selectedCard.wallet < book.price) {
+        setAlertMessage('El saldo de la tarjeta no es suficiente para realizar la compra.');
         setAlertVariant('danger');
         return;
       }
@@ -89,44 +106,12 @@ export function Shop() {
     }
   };
 
-  const handlePaymentSubmit = async (e, bookId) => {
+  const handlePaymentSubmit = async (e, bookId, selectedCard) => {
     e.preventDefault();
-  
-    // Validar el número de tarjeta
-    if (cardDetails.cardNumber.length !== 13 || isNaN(cardDetails.cardNumber)) {
-      setAlertMessage('El número de tarjeta debe ser de 13 dígitos y solo números.');
-      setAlertVariant('danger');
-      return;
-    }
-  
-    // Validar el mes de expiración
-    const expiryMonth = parseInt(cardDetails.expiryMonth);
-    if (expiryMonth < 1 || expiryMonth > 12 || isNaN(expiryMonth)) {
-      setAlertMessage('El mes de expiración debe ser un número entre 1 y 12.');
-      setAlertVariant('danger');
-      return;
-    }
-  
-    // Validar el año de expiración
-    const currentYear = new Date().getFullYear();
-    const expiryYear = parseInt(cardDetails.expiryYear);
-    if (expiryYear <= currentYear || isNaN(expiryYear)) {
-      setAlertMessage('El año de expiración debe ser un año superior al actual.');
-      setAlertVariant('danger');
-      return;
-    }
-  
-    // Validar el código de seguridad
-    if (cardDetails.securityCode.length !== 3 || isNaN(cardDetails.securityCode)) {
-      setAlertMessage('El código de seguridad debe ser un número de 3 dígitos.');
-      setAlertVariant('danger');
-      return;
-    }
-  
-    // Si pasa todas las validaciones, realizar la compra
-    await handleBuy(bookId);
+
+    // Realizar la compra
+    await handleBuy(bookId, selectedCard);
   };
-  
 
   const filteredBooks = books.filter(book =>
     book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -140,7 +125,7 @@ export function Shop() {
     <div className="container content">
       <h1 className="mt-5 mb-3">¡Bienvenido a nuestra tienda!</h1>
       <h2 className="mb-4">Explora nuestras últimas novedades</h2>
-      
+
       <div className="mb-3 row">
         <div className="col">
           <input
@@ -176,13 +161,24 @@ export function Shop() {
                 <div className="d-grid gap-2">
                   {/* Mostrar el formulario de la tarjeta solo en la tarjeta seleccionada */}
                   {selectedBookId === book.id && (
-                    <form onSubmit={(e) => handlePaymentSubmit(e, book.id)}>
-                      <input type="text" placeholder="Número de tarjeta" value={cardDetails.cardNumber || ''} onChange={(e) => setCardDetails({ ...cardDetails, cardNumber: e.target.value })} required />
-                      <input type="text" placeholder="Mes de expiración" value={cardDetails.expiryMonth || ''} onChange={(e) => setCardDetails({ ...cardDetails, expiryMonth: e.target.value })} required />
-                      <input type="text" placeholder="Año de expiración" value={cardDetails.expiryYear || ''} onChange={(e) => setCardDetails({ ...cardDetails, expiryYear: e.target.value })} required />
-                      <input type="text" placeholder="Código de seguridad" value={cardDetails.securityCode || ''} onChange={(e) => setCardDetails({ ...cardDetails, securityCode: e.target.value })} required />
-                      <button type="submit" className="btn btn-primary">Comprar</button>
-                    </form>
+                    <div>
+                      <select
+                        value={cardDetails.selectedCard ? cardDetails.selectedCard.id : ''}
+                        onChange={(e) => setCardDetails({ ...cardDetails, selectedCard: clientCards.find(card => card.id === parseInt(e.target.value)) })}
+                      >
+                        <option value="">Seleccione una tarjeta</option>
+                        {clientCards.map((card) => (
+                          <option key={card.id} value={card.id}>
+                            {card.name} (Saldo: ${card.wallet})
+                          </option>
+                        ))}
+                      </select>
+                      {cardDetails.selectedCard && (
+                        <form onSubmit={(e) => handlePaymentSubmit(e, book.id, cardDetails.selectedCard)}>
+                          <button type="submit" className="btn btn-primary">Comprar</button>
+                        </form>
+                      )}
+                    </div>
                   )}
                   {/* Mostrar el botón Comprar y cambiar el estado de selectedBookId cuando se haga clic */}
                   <button className="btn btn-primary" type="button" onClick={() => setSelectedBookId(book.id)}>Comprar</button>
