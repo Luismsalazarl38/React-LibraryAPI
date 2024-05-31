6d2bc4ec13af36de47dd687bf78205106355c0ff
Luismsalazarl38 <luis.salazar1@utp.edu.co>
2024-05-30 22:55:59 -0500

    implementacion creacion lista y eliminacion de tarjetas

--- client/src/pages/Tarjetas.jsx
+++ client/src/pages/Tarjetas.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 
 export function Tarjetas({ clientId }) {
   const [formData, setFormData] = useState({
-    client: clientId,
+    client: "",
     number: "",
     name: "",
     cv: "",
-    cadDate: ""
+    cadYear: "",
+    cadMonth: "",
+    wallet: ""
   });
   const [formOpen, setFormOpen] = useState(false);
   const [tarjetas, setTarjetas] = useState([]);
@@ -32,17 +34,29 @@ export function Tarjetas({ clientId }) {
     });
   };
 
+  const id = localStorage.getItem('id');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     try {
-      await axios.post("http://localhost:8000/manage/cards/", formData);
+      const dataToSend = {
+        ...formData,
+        client: id
+      };
+  
+      await axios.post("http://localhost:8000/manage/cards/", dataToSend, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
       alert("Tarjeta creada exitosamente");
       const response = await axios.get(`http://localhost:8000/manage/cards/?client=${clientId}`);
       setTarjetas(response.data);
       setFormOpen(false);
     } catch (error) {
-      console.error("Error al crear tarjeta:", error);
+      console.error("Error al crear tarjeta:", error.response.data);
       alert("Hubo un error al crear la tarjeta");
     }
   };
@@ -82,21 +96,30 @@ export function Tarjetas({ clientId }) {
               <input type="text" className="form-control" id="cv" name="cv" value={formData.cv} onChange={handleChange} />
             </div>
             <div className="mb-3">
-              <label htmlFor="cadDate" className="form-label">Fecha de Caducidad:</label>
-              <input type="text" className="form-control" id="cadDate" name="cadDate" value={formData.cadDate} onChange={handleChange} />
+              <label htmlFor="cadYear" className="form-label">Año de Caducidad:</label>
+              <input type="text" className="form-control" id="cadYear" name="cadYear" value={formData.cadYear} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="cadMonth" className="form-label">Mes de Caducidad:</label>
+              <input type="text" className="form-control" id="cadMonth" name="cadMonth" value={formData.cadMonth} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="wallet" className="form-label">Wallet:</label>
+              <input type="text" className="form-control" id="wallet" name="wallet" value={formData.wallet} onChange={handleChange} />
             </div>
             <button type="submit" className="btn btn-success">Crear Tarjeta</button>
           </form>
         </div>
-      )}    
+      )}
       <div className="row">
         {tarjetas.map((tarjeta) => (
           <div key={tarjeta.id} className="col-md-4 mb-4">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Cliente: {tarjeta.client}</h5>
+                {/* <h5 className="card-title">Cliente: {tarjeta.client}</h5> */}
                 <p className="card-text">Número: {tarjeta.number}</p>
-                <p className="card-text">Nombre: {tarjeta.name}</p>
+                <p className="card-text">Franquicia: {tarjeta.name}</p>
+                <p className="card-text">Saldo: {tarjeta.wallet}</p>
                 <button onClick={() => handleDelete(tarjeta.id)} className="btn btn-danger">Eliminar</button>
               </div>
             </div>
